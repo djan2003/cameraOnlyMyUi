b269fb9c0fad2da438bf17b3cfda973b60af029a
djan2003 <nikiforov777@gmail.com>
2021-03-25 21:31:31 +0300

    add button

--- src/components/Camera/Camera.tsx
+++ src/components/Camera/Camera.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import './App.css';
+import './camera.css';
 import IconButton from "@material-ui/core/IconButton";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -9,19 +9,19 @@ import Button from "@material-ui/core/Button";
 import FormGroup from "@material-ui/core/FormGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Switch from "@material-ui/core/Switch";
-import {Login} from "./components/Login/Login";
-import Camera from "./components/Camera/Camera";
 
-const App =()=>{
+function Camera() {
     const [showAllCamera, setShowAllCamera] = useState(true)
     const removeCamera =  ()=>{
         setShowAllCamera(!showAllCamera)
     }
-    return <div>
+    return (
+        <div>
+            <div>
                 <AppBar position="static">
                     <Toolbar>
                         <IconButton edge="start" color="inherit" aria-label="menu">
-                            <Menu open={false}/>
+                            <Menu open={true}/>
                         </IconButton>
                         <Typography variant="h6">
                             News
@@ -60,11 +60,13 @@ const App =()=>{
                 <Button onClick={removeCamera} color="primary">remove/add second camera</Button>
                 <button onClick={removeCamera}>remove/add second camera</button>
             </div>
+        </div>
 
 
+    );
 }
 
-export default App;
+export default Camera;
 
 
 
